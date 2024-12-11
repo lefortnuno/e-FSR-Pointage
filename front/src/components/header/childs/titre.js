a5e092a57c7d3936e849e865aa6c4974feb169d6
lefortnuno <trofelnuno@gmail.com>
2024-12-11 16:03:04 +0100

    ui ux 2

--- front/src/components/header/childs/titre.js
+++ front/src/components/header/childs/titre.js
@@ -26,7 +26,7 @@ export default function Titre({ setVal, children }) {
       <nav className="bienvenu" aria-label="breadcrumb">
         <ol className="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
           <li className="breadcrumb-item text-sm">
-            <div className="opacity-5 text-dark">e-BOA</div>
+            <div className="opacity-5 text-dark">e-FSR</div>
           </li>
           <li
             className="breadcrumb-item text-sm text-dark active"
